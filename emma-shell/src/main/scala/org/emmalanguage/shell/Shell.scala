da9f067d9482afd40b46531db1c5cdafa0d82799
Bernd Louis <bernd.louis@gmail.com>
2017-03-14 14:13:07 +0100

    Server running

---
+++ emma-shell/src/main/scala/org/emmalanguage/shell/Shell.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2014 TU Berlin (emma@dima.tu-berlin.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.emmalanguage
+package shell
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+
+import scala.io.StdIn
+
+object Shell extends App {
+  implicit val system = ActorSystem("emma-shell-system")
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  val route =
+    path("yield") {
+      get {
+        complete(s"Hello World")
+      }
+    }
+
+  val server = Http().bindAndHandle(route, "localhost", 8080)
+  println("Running…")
+  StdIn.readLine()
+
+  server.flatMap(_.unbind()).onComplete(_ => system.terminate())
+
+}
